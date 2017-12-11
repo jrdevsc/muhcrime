143cffc5d69bc4c5f0814effb6ca4850a943febf
julian rudolph <julian.rudolphsc@gmail.com>
2017-12-11 10:12:41 -0500

    shows marker of geolocaiton

--- scripts.js
+++ scripts.js
@@ -1,23 +1,3 @@
-// var map;
-// function initMap() {
-//         var uluru = {lat: -25.363, lng: 131.044};
-//         var map = new google.maps.Map(document.getElementById('map'), {
-//           zoom: 4,
-//           center: uluru
-//         });
-//         var marker = new google.maps.Marker({
-//           position: uluru,
-//           map: map
-//         });
-// };
-
-// navigator.geolocation.getCurrentPosition(function(pos){
-//   var myLat = pos.coords.latitude;
-//   var myLon = pos.coords.longitude;
-//   var myCenter = {lat: myLat, lng: myLon};
-//   initMap(myCenter);
-// });
-
 function initMap(){
   navigator.geolocation.getCurrentPosition(function(pos){
     var myLat = pos.coords.latitude;
@@ -27,6 +7,9 @@ function initMap(){
       center: myCenter,
       zoom: 8
     });
-
+    var marker = new google.maps.Marker({
+      position: myCenter,
+      map: map
+    });
   })
 }
