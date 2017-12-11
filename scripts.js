264459b15e27a23621c0b90b957f025fe014d67c
julian rudolph <julian.rudolphsc@gmail.com>
2017-12-11 10:10:29 -0500

    displays map after geolocation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
-console.log("Conn")
+// var map;
+// function initMap() {
+//         var uluru = {lat: -25.363, lng: 131.044};
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 4,
+//           center: uluru
+//         });
+//         var marker = new google.maps.Marker({
+//           position: uluru,
+//           map: map
+//         });
+// };
+
+// navigator.geolocation.getCurrentPosition(function(pos){
+//   var myLat = pos.coords.latitude;
+//   var myLon = pos.coords.longitude;
+//   var myCenter = {lat: myLat, lng: myLon};
+//   initMap(myCenter);
+// });
+
+function initMap(){
+  navigator.geolocation.getCurrentPosition(function(pos){
+    var myLat = pos.coords.latitude;
+    var myLon = pos.coords.longitude;
+    var myCenter = {lat: myLat, lng: myLon};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: myCenter,
+      zoom: 8
+    });
+
+  })
+}
